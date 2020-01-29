5594a438a8bd0776c7f6b4cbadb0aa5ff0427a4c
ANDERSON DOS SANTOS GUERRA <anderson.guerra@dinfo.br>
2020-01-29 17:32:31 -0300

    Initial implementation of websocket auth and watcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:adhara_socket_io/adhara_socket_io.dart';
-import 'package:adhara_socket_io/manager.dart';
-import 'package:adhara_socket_io/socket.dart';
+import 'package:adhara_test/models/message.dart';
 import 'package:adhara_test/services/socket.dart';
 import 'package:adhara_test/store/messages.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +8,7 @@ import 'package:provider/provider.dart';
 
 void main() => runApp(MaterialApp(
   home: MultiProvider(
-    child: Home(),
+    child: InitialScreen(),
     providers: [
       Provider<Messages>(
         create: (_) => Messages(),
@@ -19,20 +18,56 @@ void main() => runApp(MaterialApp(
   ),
 ));
 
+class InitialScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final messagesController = Provider.of<Messages>(context);
+    return FutureBuilder(
+      future: Connector.manager.createInstance(SocketOptions(
+        "http://10.0.2.2:3030/",
+        transports: [Transports.WEB_SOCKET]
+      )),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          Connector.socket = snapshot.data;
+          Connector.socket.onConnect((data) async {
+            print("on connect");
+            Connector.user = await Connector.socket.emitWithAck("create", ["authentication", {
+              "strategy": "local",
+              "email": "a@a.com",
+              "password": "123@mudar"
+            }]);
+            print(Connector.user);
+            print("conectado");
+            Connector.socket.emitWithAck("find", ["messages"]).then((response){
+              List messageList = response[0];
+              print(response);
+              messageList.forEach((message){
+                messagesController.addMessage(Message(message["text"], message["_id"]));
+              });
+            });
+          });
+          Connector.socket.connect();
+          Connector.socket.on("messages created", (message){
+            messagesController.addMessage(Message(message["text"], message["_id"]));
+          });
+          return Home();
+        }
+        return Center(child: CircularProgressIndicator(),);
+      },
+    );
+  }
+}
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  List<String> messages = [];
-  SocketIOManager manager = SocketIOManager();
-  SocketIO socket;
-
   @override
   void initState() {
     super.initState();
-    Connector();
   }
 
   @override
